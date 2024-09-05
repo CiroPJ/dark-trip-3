301e4d0caca47f5ca639a534c54df4c7b9bd1adc
Ciro Paredes <137366751+CiroPJ@users.noreply.github.com>
2024-09-05 15:06:24 -0400

    Update connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,9 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -9,6 +12,10 @@ const pool = new Pool({
   port: process.env.PGPUERTO,
   // ssl: true
 });
+
+pool.connect()
+  .then(() => console.log('Database connected!'))
+  .catch(err => console.error('Database connection error', err));
 
 try {
   pool.query("SELECT NOW()");
